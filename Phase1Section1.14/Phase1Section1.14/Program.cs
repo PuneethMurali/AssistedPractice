01e6328ab041332342c56b666c71d998e8ef8a77
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2023-12-18 07:47:25 +0530

    Add files via upload
    Changes have been made

---
+++ Phase1Section1.14/Phase1Section1.14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Phase1Section1._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\mural\\source\\repos\\Phase1Section1.14\\Phase1Section1.14\\students.txt";
+
+            if(File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found or inaccessible.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
