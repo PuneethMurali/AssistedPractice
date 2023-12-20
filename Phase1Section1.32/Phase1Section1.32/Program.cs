c3acbf8fbcc0a1b7effb4ebdffa0f55b094185be
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2023-12-20 08:16:41 +0530

    Changes have been made

---
+++ Phase1Section1.32/Phase1Section1.32/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section1._32
+{
+    internal class Program
+    {
+        static List<Student> students = new List<Student>();
+
+        static void Main(string[] args)
+        {
+            ReadStudentDataFromFile(); 
+            SortStudentsByName();     
+
+            DisplayStudents();        
+
+            SearchStudentByName();    
+        }
+
+        static void ReadStudentDataFromFile()
+        {
+            try
+            {
+                string filePath = "C:\\Users\\mural\\source\\repos\\Phase1Section1.32\\Phase1Section1.32\\students.txt"; // Path to your text file
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(',');
+
+                    if (data.Length >= 2)
+                    {
+                        Student student = new Student
+                        {
+                            Name = data[0].Trim(),
+                            Class = data[1].Trim()
+                        };
+                        students.Add(student);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading file: " + ex.Message);
+            }
+        }
+
+        static void SortStudentsByName()
+        {
+            students = students.OrderBy(s => s.Name).ToList();
+        }
+
+        static void DisplayStudents()
+        {
+            Console.WriteLine("Sorted Student Data:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name}, {student.Class}");
+            }
+            Console.WriteLine();
+        }
+
+        static void SearchStudentByName()
+        {
+            Console.WriteLine("Enter student name to search:");
+            string searchName = Console.ReadLine().Trim();
+
+            var foundStudents = students.Where(s => s.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase));
+
+            if (foundStudents.Any())
+            {
+                Console.WriteLine("Student(s) Found:");
+                foreach (var student in foundStudents)
+                {
+                    Console.WriteLine($"{student.Name}, {student.Class}");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Student not found.");
+                
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+}
+
---
+++ Phase9Section1.14/Phase9Section1.14/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase9Section1._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            List<Teacher> teachers = new List<Teacher>();
+            List<Subject> subjects = new List<Subject>();
+
+            PopulateData(students, teachers, subjects);
+
+            DisplayStudentsInClass(students, "Class A");
+
+            DisplaySubjectsByTeacher(subjects, teachers[0]);
+
+            Console.ReadLine();
+        }
+
+        static void PopulateData(List<Student> students, List<Teacher> teachers, List<Subject> subjects)
+        {
+
+            students.Add(new Student("Alex", "Class A"));
+            students.Add(new Student("Balbir", "Class B"));
+            students.Add(new Student("Christopher", "Class A"));
+
+
+            teachers.Add(new Teacher("Mr. Antony", "Class A"));
+            teachers.Add(new Teacher("Mrs. Tina", "Class B"));
+
+
+            subjects.Add(new Subject("Maths", "MATH101", teachers[0]));
+            subjects.Add(new Subject("Science", "SCI101", teachers[1]));
+        }
+
+        static void DisplayStudentsInClass(List<Student> students, string className)
+        {
+            Console.WriteLine($"Students in {className}:");
+            foreach (var student in students)
+            {
+                if (student.Class.Equals(className, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"- {student.Name}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplaySubjectsByTeacher(List<Subject> subjects, Teacher teacher)
+        {
+            Console.WriteLine($"Subjects taught by {teacher.Name}:");
+            foreach (var subject in subjects)
+            {
+                if (subject.Teacher.Equals(teacher))
+                {
+                    Console.WriteLine($"- {subject.Name} ({subject.SubjectCode})");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        public Student(string name, string studentClass)
+        {
+            Name = name;
+            Class = studentClass;
+        }
+    }
+
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        public Teacher(string name, string teacherClass)
+        {
+            Name = name;
+            Class = teacherClass;
+        }
+    }
+
+    public class Subject
+    {
+        public string Name { get; set; }
+        public string SubjectCode { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public Subject(string name, string code, Teacher teacher)
+        {
+            Name = name;
+            SubjectCode = code;
+            Teacher = teacher;
+        }
+    }
+}
+
+
+
+
