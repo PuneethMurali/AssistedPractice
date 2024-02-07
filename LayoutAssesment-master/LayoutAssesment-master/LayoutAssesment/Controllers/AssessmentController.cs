5c162704bc1c2a891927a04c801716a7eecfe827
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2024-02-07 18:29:56 +0530

    Commit

---
+++ LayoutAssesment-master/LayoutAssesment-master/LayoutAssesment/Controllers/AssessmentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutAssesment.Controllers
+{
+    public class AssessmentController : Controller
+    {
+        // GET: Assessment
+        public ActionResult Index()
+        {
+            List<String> Names = new List<String>
+            {
+                "sam","felix","laurence"
+            };
+            ViewBag.CNames = Names;
+            return View();
+        }
+        public ActionResult DisplayMessage()
+        {
+            var message = TempData["message"] as string;
+            ViewBag.Message = message;
+            return View();
+        }
+        public ActionResult RedirectDemo()
+        {
+            TempData["message"] = " Welcome! Here are our valued customers:";
+            return RedirectToAction("DisplayMessage");
+        }
+      
+
+
+
+
+
+    }
+}
